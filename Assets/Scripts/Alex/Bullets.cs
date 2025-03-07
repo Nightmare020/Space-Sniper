7b5809cb622b17160f5536d95476341ba66ea2f3
Nightmare020 <albalexbelen@gmail.com>
2025-03-07 13:26:51 +0000

    Bullets Count Working

--- Assets/Scripts/Alex/Bullets.cs
+++ Assets/Scripts/Alex/Bullets.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Bullets : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Array to hold the bullet GameObjects
+    public GameObject[] bulletImages;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check if the Fire1 button is pressed
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        // Check if there are any bullets left
+        if (bulletImages.Length > 0)
+        {
+            // Get the last bullet in the array
+            GameObject lastBullet = bulletImages[bulletImages.Length - 1];
+
+            // Destroy the last bullet GameObject
+            Destroy(lastBullet);
+
+            // Remove the last bullet from the array
+            List<GameObject> bulletList = new List<GameObject>(bulletImages);
+            bulletList.RemoveAt(bulletList.Count - 1);
+            bulletImages = bulletList.ToArray();
+        }
     }
 }
